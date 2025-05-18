24fe054bdb62d59f751bb0940df18219feebb87f
Knowny <tomas.husar4401@gmail.com>
2025-05-18 21:49:02 +0200

    adding comments for better readability

--- alignment/align_faces.py
+++ alignment/align_faces.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""
+This code aligns face images from the src dataset and creates CSV metadata file.
+note: Multiple detected faces from one image are saved as two separate images.
+
+filename: align_faces.py
+project: KNN Face Recognition
+version: 1.0
+author: Tomas Husar, xhusar11
+"""
 import os
 from pathlib import Path
 from ultralytics import YOLO
@@ -9,7 +19,7 @@ from face_alignment import allign_face
 
 # * CONFIGURATION
 DETECTOR_PATH = "../archival_faces_detector/ArchivalFaces_2024_08_07_fold_0_yolo11l.pt"
-CROP_SIZE = (112, 112)  # * SET THE CROP SIZE ACCORDING TO MODEL NEEDS - needs fix with other sizes
+CROP_SIZE = (112, 112)
 DATASET_PATH = "../datasets/stylized_images"
 ALIGNED_DATASET_PATH = "../datasets/stylized_images_aligned_112"
 OUTPUT_CSV = "../datasets/stylized_images_aligned_112/aligned_faces_metadata.csv"
@@ -43,6 +53,7 @@ with open(OUTPUT_CSV, mode="w", newline="", encoding="utf-8") as csvfile:
             print(f"Skipping {image_path}: {e}")
             continue
 
+        # Get results from YOLO face detector
         results = detector(image, verbose=False)
         if not results:
             continue
@@ -51,26 +62,31 @@ with open(OUTPUT_CSV, mode="w", newline="", encoding="utf-8") as csvfile:
         keypoints = result.keypoints
         confidences = result.boxes.conf.tolist()
 
+        # Skip if no keypoints detected
         if keypoints is None or len(keypoints) == 0:
             continue
 
-        keypoints = keypoints.xy  # shape [N, 5, 2]
+        keypoints = keypoints.xy    # shape [N, 5, 2] (N faces, 5 keypoints per face)
 
         aligned_identity_folder = os.path.join(ALIGNED_DATASET_PATH, identity)
         os.makedirs(aligned_identity_folder, exist_ok=True)
 
         base_name = os.path.splitext(os.path.basename(image_path))[0]
 
+        # Process each detected face in the image
         for idx, (face_keypoints, confidence) in enumerate(zip(keypoints.data[:, :, :2], confidences), 1):
             try:
+                # Align the face using 5 keypoints
                 aligned_face = allign_face(image, None, face_keypoints, CROP_SIZE)
                 if aligned_face is None:
                     raise ValueError("allign_face returned None")
 
+                # Save the face
                 aligned_filename = f"{base_name}_{idx:02d}.jpg"
                 aligned_path = os.path.join(aligned_identity_folder, aligned_filename)
                 aligned_face.save(aligned_path)
 
+                # Write to CSV
                 csvwriter.writerow([
                     identity,
                     os.path.relpath(image_path, DATASET_PATH),
